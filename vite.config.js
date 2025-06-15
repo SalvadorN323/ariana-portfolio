29318b3d7838858334b652cbe37f5a071137f7be
Salvador Nunez <snunez0043@gmail.com>
2025-06-15 13:23:08 -0700

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -5,16 +5,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/', // Replace with your repository name
-  build: {
-    outDir: 'build',
-    chunkSizeWarningLimit: 1000, // Increase the warning limit to 1000kb
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-          mui: ['@mui/material', '@mui/icons-material'],
-        },
-      },
-    },
-  },
 })
